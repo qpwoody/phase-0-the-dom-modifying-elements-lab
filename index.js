11ebf42a2bc9144da9608b798d27546c9ea0bcb2
quinn woodhouse <qpwoodhouse@gmail.com>
2023-10-15 01:26:27 -0600

    Dom Mod Elements Lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const mainElement = document.querySelector('main#main');
+ mainElement.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+
+newHeader.textContent = "QUINN is the champion";
+
+document.body.appendChild(newHeader);
